a55562f793c6e5caa64bc7341a84e1a707047815
Reynier124 <reynier1306@gmail.com>
2025-04-25 09:05:17 -0300

    Creadas pruebas para catalogo

--- TP3/src/main/java/org/example/Main.java
+++ TP3/src/main/java/org/example/Main.java
@@ -2,13 +2,8 @@ package org.example;
 
 
 import org.example.Model.Libro;
-import org.example.Util.UniqueISBNGenerator;
-import org.example.Enum.Estado;
 
 public class Main {
     public static void main(String[] args) {
-        UniqueISBNGenerator isbnGenerator = new UniqueISBNGenerator();
-        Libro l1 = new Libro(isbnGenerator.generateUniqueISBN13(), "Harry potter", "J.K Rowling");
-        System.out.println(l1.toString());
     }
 }
