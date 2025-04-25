79ddbc3a34c99a421ca41922176a23f93fe1adb3
Reynier124 <reynier1306@gmail.com>
2025-04-25 08:49:32 -0300

    Creacion de pruebas y refactorización de libro

--- TP3/src/main/java/org/example/Main.java
+++ TP3/src/main/java/org/example/Main.java
@@ -8,9 +8,7 @@ import org.example.Enum.Estado;
 public class Main {
     public static void main(String[] args) {
         UniqueISBNGenerator isbnGenerator = new UniqueISBNGenerator();
-        Libro l1 = new Libro(isbnGenerator.generateUniqueISBN13(), "Harry potter", "J.K Rowling", Estado.DISPONIBLE);
+        Libro l1 = new Libro(isbnGenerator.generateUniqueISBN13(), "Harry potter", "J.K Rowling");
         System.out.println(l1.toString());
-
-
     }
 }
--- TP3/src/main/java/org/example/Model/Libro.java
+++ TP3/src/main/java/org/example/Model/Libro.java
@@ -1,17 +1,19 @@
 package org.example.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.example.Enum.Estado;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class Libro {
     private String ISBN;
     private String titulo;
     private String autor;
-    private Estado estado;
+    private Estado estado = Estado.DISPONIBLE;
 
+    public Libro(String ISBN, String titulo, String autor) {
+        this.ISBN = ISBN;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
 }
