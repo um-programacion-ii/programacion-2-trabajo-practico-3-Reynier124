c8f58d79308476ec5fb20ac594d9fc57a78d2964
Reynier124 <reynier1306@gmail.com>
2025-04-25 10:33:03 -0300

    Creado sistema de prestamos

---
+++ TP3/src/main/java/org/example/Model/SistemaPrestamos.java
@@ -0,0 +1,42 @@
+package org.example.Model;
+
+import lombok.Data;
+import org.example.Enum.Estado;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class SistemaPrestamos {
+    private Catalogo catalogo;
+    private List<Prestamo> prestamos = new ArrayList<>();
+
+    public SistemaPrestamos(Catalogo catalogo) {
+        this.catalogo = catalogo;
+    }
+
+    public Prestamo prestarLibro(String isbn){
+        Libro libro = catalogo.buscarPorIsbn(isbn);
+        if (libro.getEstado() == Estado.DISPONIBLE){
+            Prestamo prestamo = new Prestamo(LocalDate.now(), libro);
+            prestamos.add(prestamo);
+            libro.setEstado(Estado.PRESTADO);
+            return prestamo;
+        }else {
+            System.out.println("El libro ya está prestado");
+            return null;
+        }
+
+    }
+
+    public void devolverLibro(String isbn){
+        Libro libro = catalogo.buscarPorIsbn(isbn);
+        if (libro.getEstado() == Estado.PRESTADO){
+            prestamos.remove(libro);
+            libro.setEstado(Estado.DISPONIBLE);
+        }else {
+            System.out.println("El libro no está prestado");
+        }
+    }
+}
--- TP3/src/test/java/PrestamosTest.java
+++ TP3/src/test/java/PrestamosTest.java
@@ -1,8 +1,7 @@
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import org.example.Enum.Estado;
 import org.example.Model.Catalogo;
@@ -52,7 +51,7 @@ public class PrestamosTest {
         sistemaPrestamos.devolverLibro("978-3-16-148410-0");
 
         assertEquals(Estado.DISPONIBLE, libro.getEstado());
-        verify(catalogo).buscarPorIsbn("978-3-16-148410-0");
+        verify(catalogo, times(2)).buscarPorIsbn("978-3-16-148410-0");
 
     }
 
